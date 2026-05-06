12b19398535962555e3d3dbb190eda0ddbc8f2a6
anass el ghalbzouri <69629520+anassreda@users.noreply.github.com>
2026-05-06 16:48:52 +0100

    Fix GitHub Pages basePath and assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/pastry-portfolio",
+  assetPrefix: "/pastry-portfolio",
   images: {
     unoptimized: true,
     remotePatterns: [
